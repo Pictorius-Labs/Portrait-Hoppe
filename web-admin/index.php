89012f27eb9e09acd1e363b3c5b48c7fc7af6350
Marcus Richert <windows8user@web.de>
2017-09-18 20:07:11 +0200

    Verbesserungen am Admin Panel

--- web-admin/index.php
+++ web-admin/index.php
@@ -1,62 +1,46 @@
 <?php
-  if(isset($_GET["site"]))
-  {
-    $site = $_GET["site"];
-  }
-  else
-  {
-    $site = "";
-  }
-?>
+    if(isset($_GET["site"])){
+            $site = $_GET["site"];
+            }else
+            {
+                $site = "";
+            }
 
+    require_once '../mysql.php';
+    $db = new DB();
+    $LoggedIn = false;
+    if(isset($_POST['einloggen'])) {
+            $user = $_POST['benutzername'];
+            $passwort = hash('sha512', $_POST['passwort'] . $user);
+            if($db->login($user, $passwort) === TRUE) { 
+                $LoggedIn = true;
+                setcookie("USR",$passwort,time()+(1800));
+            } else {
+                $errorMessage = "<div id='login_fail'>Anmeldung fehlgeschlagen!</div>";	
+            }
+        }
+    $logged = $db->LoggedIn();
+    foreach ($logged as $row) {
+    if (isset($_COOKIE["USR"]) && $row["passwort"] == $_COOKIE["USR"]) {
+            $LoggedIn = true;
+        }
+    }
+?>
 <html>
-  <head>
+    <head>
     <title>Bildhauer Burkhard Hoppe - Administrator Bereich</title>
-    <meta charset="UTF-8">
+    
+    <meta charset="utf-8">
     <link rel="stylesheet" type="text/css" href="style_admin.css">
     <link href="https://fonts.googleapis.com/css?family=Poppins:100,300" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Lato:300,400" rel="stylesheet" type="text/css">
-  </head>
-    
-  <?php
-    require_once '../mysql.php';
-        
-    $db = new DB();
-    $LoggedIn = false;
-    
-    if(isset($_POST['einloggen'])) 
-    {
-      $user = $_POST['benutzername'];
-      $passwort = hash('sha512', $_POST['passwort'] . $user);
-      
-      if($db->login($user, $passwort) === TRUE) 
-      { 
-        $LoggedIn = true;
-        setcookie("USR",$passwort,time()+(1800));
-      } 
-      else 
-      {
-        echo "<style>#login_fail {display: block;}</style>";	
-      }
-    }
-    
-    $logged = $db->LoggedIn();
-    
-    foreach ($logged as $row) 
-    {
-      if (isset($_COOKIE["USR"]) && $row["passwort"] == $_COOKIE["USR"]) 
-      {
-        $LoggedIn = true;
-      }
-    }
-    
-    if(!$LoggedIn) 
-    {
-  ?>
-  
-  <body>
+    </head>
+    <?php
+        if(!$LoggedIn) {
+    ?>
+    <body>
     <div class="login_bereich">
-      <center>
+    <center>
         <img src="../img/logo.gif">
         <p>Administrator - Bereich</p>
 
@@ -65,35 +49,34 @@
             <br>
             <input type="password" placeholder="Passwort" name="passwort" required>
             <br>
-            <div id="login_fail">Anmeldung fehlgeschlagen!</div>
+            <?php
+            if(isset($errorMessage)) {
+                echo $errorMessage;
+            }
+            ?>
             <input type="submit" name="einloggen" value="Anmelden">
         </form>
-        
-      </center>
+    </center>
     </div>
-    
-  <?php
+    <?php
     }
 
-    if($LoggedIn) 
-    {
-  ?>
-    
-  <div class="menu">
-    <img src="img/logo.png">
-    <p>Administrator - Bereich</p>  
-    <div class="menu_object"><a href="?site=neuigkeiten">Neuigkeiten</a></div>
-    <div class="menu_object"><a href="?site=kurse">Kurse</a></div>
-    <div class="menu_object"><a href="?site=shop">Shop</a></div>
-    <div class="menu_object"><a href="logout.php">Abmelden</a></div>
-  </div>
-  <div class="content">
-    <?php include("sites.php");?>
-  </div>
-  
-  <?php
+    if($LoggedIn) {
+    ?>
+    <div class="menu">
+            <img src="img/logo.png">
+            <p>Administrator - Bereich</p>
+        
+        <div class="menu_object"><a href="?site=neuigkeiten">Neuigkeiten</a></div>
+        <div class="menu_object"><a href="?site=kurse">Kurse</a></div>
+        <div class="menu_object"><a href="?site=shop">Shop</a></div>
+        <div class="menu_object"><a href="logout.php">Abmelden</a></div>
+    </div>
+    <div class="content">
+        <?php include("sites.php");?>
+    </div>
+    <?php
     }
-  ?>
-    
-  </body>
+    ?>
+    </body>
 </html>
